403a8dd2532d142dbfc7912289726b4f1d1ec634
Binhnt <binh.thanhnhu@gmail.com>
2017-06-11 22:52:33 +0700

    update for testing

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const io = require('socket.io')(process.env.PORT || 3000);
-
 const arrUserInfo = [];
 
 io.on('connection', socket => {
@@ -9,17 +8,17 @@ io.on('connection', socket => {
         if (isExist) return socket.emit('DUPLICATE');
         arrUserInfo.push(user);
         io.emit('NEW', user);
-        console.log(arrUserInfo);
+        console.log(`consultant login ${user}`);
     });
     socket.on('GET_LIST_USER', () => {
-        console.log(arrUserInfo);
         socket.emit('USER_ARRAY', arrUserInfo);
     });
     socket.on('CONNECT_USER', user => {
         socket.peerIdCaller = user.peerIdCaller;
-        const index = arrUserInfo.findIndex(user => user.peerId === socket.peerIdAnswer);
-        arrUserInfo.splice(index, 1);
-        io.emit('USER_BUSY', user)
+        const index = arrUserInfo.findIndex(e => e.peerId === user.peerId);
+        arrUserInfo[index].busy = true;
+        io.emit('USER_BUSY', user);
+        socket.emit('PEER_ID_CALLER', user.peerIdCaller);
     });
     socket.on('disconnect', () => {
         if (socket.peerIdAnswer) {
@@ -27,14 +26,5 @@ io.on('connection', socket => {
             arrUserInfo.splice(index, 1);
             io.emit('USER_OFFLINE', socket.peerIdAnswer)
         }
-        // else {
-        //     arrUserInfo.forEach(user => {
-        //         if (user.peerIdCaller === socket.peerIdCaller) {
-        //             console.log(12312);
-        //             arrUserInfo.push(user);
-        //             return io.emit('NEW', user)
-        //         }
-        //     });
-        // }
     })
 });
