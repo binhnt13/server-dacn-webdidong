b6d4ca20ffd3e86d63d5bda7f026628c5d58470e
Binhnt <binh.thanhnhu@gmail.com>
2017-06-11 18:44:18 +0700

    Update for test

--- index.js
+++ index.js
@@ -5,18 +5,36 @@ const arrUserInfo = [];
 io.on('connection', socket => {
     socket.on('REGISTER', user => {
         const isExist = arrUserInfo.some(e => e.ten === user.ten);
-        socket.peerId = user.peerId;
+        socket.peerIdAnswer = user.peerId;
         if (isExist) return socket.emit('DUPLICATE');
         arrUserInfo.push(user);
         io.emit('NEW', user);
+        console.log(arrUserInfo);
     });
-    socket.on('RECEIVE_CALL', callee => {
-        console.log(callee);
+    socket.on('GET_LIST_USER', () => {
+        console.log(arrUserInfo);
         socket.emit('USER_ARRAY', arrUserInfo);
     });
-    socket.on('disconnect', () => {
-        const index = arrUserInfo.findIndex(user => user.peerId === socket.peerId);
+    socket.on('CONNECT_USER', user => {
+        socket.peerIdCaller = user.peerIdCaller;
+        const index = arrUserInfo.findIndex(user => user.peerId === socket.peerIdAnswer);
         arrUserInfo.splice(index, 1);
-        io.emit('USER_DISCONNECT', socket.peerId)
+        io.emit('USER_BUSY', user)
+    });
+    socket.on('disconnect', () => {
+        if (socket.peerIdAnswer) {
+            const index = arrUserInfo.findIndex(user => user.peerId === socket.peerIdAnswer);
+            arrUserInfo.splice(index, 1);
+            io.emit('USER_OFFLINE', socket.peerIdAnswer)
+        }
+        // else {
+        //     arrUserInfo.forEach(user => {
+        //         if (user.peerIdCaller === socket.peerIdCaller) {
+        //             console.log(12312);
+        //             arrUserInfo.push(user);
+        //             return io.emit('NEW', user)
+        //         }
+        //     });
+        // }
     })
 });
